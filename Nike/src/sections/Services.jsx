0c8d0ed2f3975630fd35f3d32be9fd3a43361310
sanjay <sanjayshre4815@gmail.com>
2024-11-12 11:44:29 +0530

    add Nike

---
+++ Nike/src/sections/Services.jsx
@@ -0,0 +1,16 @@
+import ServicesCard from "../components/ServicesCard"
+import { services } from "../constants"
+
+const Services = () => {
+  return (
+    <section className="max-container flex justify-center flex-wrap gap-9">
+      {
+        services.map((service) => (
+          <ServicesCard key={service.label} {...service} />
+        ))
+      }
+    </section>
+  )
+}
+
+export default Services
---
+++ Nike/src/sections/index.js
@@ -0,0 +1,10 @@
+import CustomerReview from "./CustomerReview"
+import Footer from "./Footer"
+import Hero from "./Hero"
+import PopularProduct from "./PopularProduct"
+import Services from "./Services"
+import SpecialOffers from "./SpecialOffers"
+import Subscribe from "./Subscribe"
+import SuperQuality from "./SuperQuality"
+
+export {CustomerReview, Footer, Hero, PopularProduct, Services, SpecialOffers, Subscribe, SuperQuality }
